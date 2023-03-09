11e221bc43f187fb22237c2c098818c02feaa742
suvra2001 <112110876+suvra2001@users.noreply.github.com>
2023-03-09 00:30:54 -0500

    project updates
    - redid projects tab
    - added buttons to case studies
    - started case studies

--- js/index.js
+++ js/index.js
@@ -12,3 +12,29 @@ $('.navProj').on("click", function(e){
       scrollTop: $(targetHref).offset().top
   }, 1000);
 });
+
+// Get all the cards on the page
+const cards = document.querySelectorAll('.card');
+
+// Define the function to check if a card is in view
+function isCardVisible(card) {
+  const rect = card.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Define the function to handle scroll events
+function handleScroll() {
+  cards.forEach(card => {
+    if (isCardVisible(card)) {
+      card.classList.add('animation');
+    }
+  });
+}
+
+// Listen for scroll events and call the handleScroll function
+window.addEventListener('scroll', handleScroll);
